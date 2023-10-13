ae7c1c190df8b199e2898dcefc5a68ad2f6977b4
tayian <90464339+tayian@users.noreply.github.com>
2023-10-13 12:29:56 +0800

    Refactor DeleteCommandParser.java to DeletePersonCommandParser.java

--- src/main/java/connectify/logic/parser/DeletePersonCommandParser.java
+++ src/main/java/connectify/logic/parser/DeletePersonCommandParser.java
@@ -3,26 +3,26 @@ package connectify.logic.parser;
 import static connectify.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import connectify.commons.core.index.Index;
-import connectify.logic.commands.DeleteCommand;
+import connectify.logic.commands.DeletePersonCommand;
 import connectify.logic.parser.exceptions.ParseException;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
  */
-public class DeleteCommandParser implements Parser<DeleteCommand> {
+public class DeletePersonCommandParser implements Parser<DeletePersonCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
+    public DeletePersonCommand parse(String args) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            return new DeletePersonCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePersonCommand.MESSAGE_USAGE), pe);
         }
     }
 
