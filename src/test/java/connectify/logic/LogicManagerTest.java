91cdc9a181e9b900534deefe1bd7355340544cf9
tayian <90464339+tayian@users.noreply.github.com>
2023-10-13 17:54:24 +0800

    Keep linelength within 120 char in LogicManagerTest.java

--- src/test/java/connectify/logic/LogicManagerTest.java
+++ src/test/java/connectify/logic/LogicManagerTest.java
@@ -168,7 +168,8 @@ public class LogicManagerTest {
         logic = new LogicManager(model, storage);
 
         // Triggers the saveAddressBook method by executing an add command
-        String addPersonCommand = AddPersonCommand.COMMAND_WORD + CommandTestUtil.NAME_DESC_AMY + CommandTestUtil.PHONE_DESC_AMY
+        String addPersonCommand = AddPersonCommand.COMMAND_WORD
+                + CommandTestUtil.NAME_DESC_AMY + CommandTestUtil.PHONE_DESC_AMY
                 + CommandTestUtil.EMAIL_DESC_AMY + CommandTestUtil.ADDRESS_DESC_AMY;
         Person expectedPerson = new PersonBuilder(TypicalPersons.AMY).withTags().build();
         ModelManager expectedModel = new ModelManager();
