2ddf3a0945aa4d99254d82f9c71bdef10214ae25
geraldngjx <geraldngjx@gmail.com>
2023-10-25 21:14:48 +0800

    Add NoteCommandParser

--- src/main/java/connectify/logic/parser/NoteCommandParser.java
+++ src/main/java/connectify/logic/parser/NoteCommandParser.java
@@ -1,4 +1,37 @@
 package connectify.logic.parser;
 
-public class NoteCommandParser {
+import static java.util.Objects.requireNonNull;
+import static connectify.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static connectify.logic.parser.CliSyntax.PREFIX_NOTE;
+
+import connectify.commons.core.index.Index;
+import connectify.commons.exceptions.IllegalValueException;
+import connectify.logic.commands.NoteCommand;
+import connectify.logic.parser.exceptions.ParseException;
+import connectify.model.Note;
+
+/**
+ * Parses input arguments and creates a new {@code NoteCommand} object
+ */
+public class NoteCommandParser implements Parser<NoteCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code NoteCommand}
+     * and returns a {@code NoteCommand} object for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    public NoteCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NOTE);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE), ive);
+        }
+
+        String note = argMultimap.getValue(PREFIX_NOTE).orElse("");
+
+        return new NoteCommand(index, new Note(note));
+    }
 }
