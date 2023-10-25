ff1258f6f2be0dbef55a84f808001365e69fe255
geraldngjx <geraldngjx@gmail.com>
2023-10-25 21:56:33 +0800

    Add tests for NoteCommand

--- src/test/java/connectify/logic/commands/NoteCommandTest.java
+++ src/test/java/connectify/logic/commands/NoteCommandTest.java
@@ -1,2 +1,134 @@
-package connectify.logic.commands;public class NoteCommandTest {
+package connectify.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static connectify.logic.commands.CommandTestUtil.VALID_NOTE_AMY;
+import static connectify.logic.commands.CommandTestUtil.VALID_NOTE_BOB;
+import static connectify.logic.commands.CommandTestUtil.assertCommandFailure;
+import static connectify.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static connectify.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static connectify.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static connectify.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static connectify.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import connectify.logic.Messages;
+import connectify.commons.core.index.Index;
+import connectify.model.AddressBook;
+import connectify.model.Model;
+import connectify.model.ModelManager;
+import connectify.model.UserPrefs;
+import connectify.model.Note;
+import connectify.model.person.Person;
+import connectify.testutil.PersonBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for NoteCommand.
+ */
+public class NoteCommandTest {
+    private static final String NOTE_STUB = "Some note";
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_addNoteUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(firstPerson).withNote(NOTE_STUB).build();
+
+        NoteCommand noteCommand = new NoteCommand(INDEX_FIRST_PERSON,
+                new Note(editedPerson.getNote().getContent()));
+
+        String expectedMessage = String.format(NoteCommand.MESSAGE_ADD_NOTE_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(noteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_deleteNoteUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(firstPerson).withNote("").build();
+
+        NoteCommand noteCommand = new NoteCommand(INDEX_FIRST_PERSON,
+                new Note(editedPerson.getNote().toString()));
+
+        String expectedMessage = String.format(NoteCommand.MESSAGE_DELETE_NOTE_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(noteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()))
+                .withNote(NOTE_STUB).build();
+
+        NoteCommand noteCommand = new NoteCommand(INDEX_FIRST_PERSON,
+                new Note(editedPerson.getNote().getContent()));
+
+        String expectedMessage = String.format(NoteCommand.MESSAGE_ADD_NOTE_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(noteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        NoteCommand noteCommand = new NoteCommand(outOfBoundIndex, new Note(VALID_NOTE_BOB));
+
+        assertCommandFailure(noteCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where the index is larger than the size of the filtered list,
+     * but smaller than the size of the address book.
+     */
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // Ensures that outOfBoundIndex is still in the bounds of the address book list.
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        NoteCommand noteCommand = new NoteCommand(outOfBoundIndex, new Note(VALID_NOTE_BOB));
+
+        assertCommandFailure(noteCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        final NoteCommand standardCommand = new NoteCommand(INDEX_FIRST_PERSON,
+                new Note(VALID_NOTE_AMY));
+        // Same values -> returns true
+        NoteCommand commandWithSameValues = new NoteCommand(INDEX_FIRST_PERSON,
+                new Note(VALID_NOTE_AMY));
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // Same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // Null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // Different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // Different index -> returns false
+        assertFalse(standardCommand.equals(new NoteCommand(INDEX_SECOND_PERSON,
+                new Note(VALID_NOTE_AMY))));
+        // Different note -> returns false
+        assertFalse(standardCommand.equals(new NoteCommand(INDEX_FIRST_PERSON,
+                new Note(VALID_NOTE_BOB))));
+    }
 }
