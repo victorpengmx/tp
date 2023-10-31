dfd11d89f0656615cf669c09e5a920bd3a6ff027
Victor <vpeng36@gmail.com>
2023-10-31 22:32:29 +0800

    Add tests for Person comparators

--- src/test/java/connectify/model/person/PersonNameComparatorTest.java
+++ src/test/java/connectify/model/person/PersonNameComparatorTest.java
@@ -1,5 +1,9 @@
 package connectify.model.person;
 
+import static connectify.testutil.TypicalPersons.ALICE;
+import static connectify.testutil.TypicalPersons.BOB;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Comparator;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +15,9 @@ public class PersonNameComparatorTest {
 
     @Test
     public void compare() {
-
+        Comparator<Person> comparator = new PersonNameComparator();
+        assertEquals(comparator.compare(ALICE, BOB), -1);
+        assertEquals(comparator.compare(ALICE, ALICE), 0);
+        assertEquals(comparator.compare(BOB, ALICE), 1);
     }
 }
--- src/test/java/connectify/model/person/PersonPriorityComparatorTest.java
+++ src/test/java/connectify/model/person/PersonPriorityComparatorTest.java
@@ -1,20 +1,23 @@
 package connectify.model.person;
 
+import static connectify.testutil.TypicalPersons.ALICE;
+import static connectify.testutil.TypicalPersons.BOB;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Comparator;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Compares a {@code Person} to another using their {@code Priority}.
  */
-public class PersonPriorityComparatorTest implements Comparator<Person> {
+public class PersonPriorityComparatorTest {
 
-    @Override
-    public int compare(Person o1, Person o2) {
-        if (o1.getPriority().getValue() < o2.getPriority().getValue()) {
-            return -1;
-        } else if (o1.getPriority().getValue() > o2.getPriority().getValue()) {
-            return 1;
-        } else {
-            return 0;
-        }
+    @Test
+    public void compare() {
+        Comparator<Person> comparator = new PersonPriorityComparator();
+        assertEquals(comparator.compare(ALICE, BOB), 1);
+        assertEquals(comparator.compare(ALICE, ALICE), 0);
+        assertEquals(comparator.compare(BOB, ALICE), -1);
     }
 }
