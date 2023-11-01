54ad7c6afb8be5b14f0849b2a091acdeb20e69a5
tayian <90464339+tayian@users.noreply.github.com>
2023-11-01 17:05:22 +0800

    Update SharePersonCommandTest.java

--- src/test/java/connectify/logic/commands/SharePersonCommandTest.java
+++ src/test/java/connectify/logic/commands/SharePersonCommandTest.java
@@ -1,13 +1,14 @@
 package connectify.logic.commands;
 
+import static connectify.logic.commands.CommandTestUtil.assertCommandFailure;
 import static connectify.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static connectify.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static connectify.testutil.TypicalIndexes.INDEX_FIRST_COMPANY;
 import static connectify.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static connectify.testutil.TypicalPersons.getTypicalAddressBook;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import connectify.commons.core.index.Index;
@@ -18,83 +19,59 @@ import connectify.model.UserPrefs;
 import connectify.model.person.Person;
 
 /**
- * Contains integration tests (interaction with the Model) and unit tests for SharePersonCommand.
+ * Contains integration tests (interaction with the Model) and unit tests for
+ * {@code SharePersonCommand}.
  */
 public class SharePersonCommandTest {
-    private Model model;
-    private Model expectedModel;
 
-    @BeforeEach
-    public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-    }
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_validIndexUnfiltered_showsShareSuccessMessage() {
-        Index index = INDEX_FIRST_PERSON;
-        Person personToShare = model.getFilteredPersonList().get(index.getZeroBased());
+    public void execute_validIndexesUnfilteredList_success() {
+        Person personToShare = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        SharePersonCommand sharePersonCommand = new SharePersonCommand(INDEX_FIRST_COMPANY, INDEX_FIRST_PERSON);
 
-        SharePersonCommand shareCommand = new SharePersonCommand(index);
         String expectedMessage = String.format(SharePersonCommand.MESSAGE_SHARE_PERSON_SUCCESS,
                 SharePersonCommand.formatInput(Messages.format(personToShare)));
 
-        assertCommandSuccess(shareCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(sharePersonCommand, model, expectedMessage, model);
+
+        // Additional checks if the person is still in the original list
+        assertTrue(model.getFilteredPersonList().contains(personToShare));
     }
 
     @Test
-    public void execute_invalidIndexUnfiltered_throwsCommandException() {
-        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-        SharePersonCommand shareCommand = new SharePersonCommand(outOfBoundIndex);
+    public void execute_invalidPersonIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundPersonIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        SharePersonCommand sharePersonCommand = new SharePersonCommand(INDEX_FIRST_COMPANY, outOfBoundPersonIndex);
 
-        CommandTestUtil.assertCommandFailure(shareCommand, model,
-                Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-    }
-
-    @Test
-    public void execute_validIndexFiltered_showsShareSuccessMessage() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-
-        Index index = INDEX_FIRST_PERSON;
-        Person personToShare = model.getFilteredPersonList().get(index.getZeroBased());
-
-        SharePersonCommand shareCommand = new SharePersonCommand(index);
-        String expectedMessage = String.format(SharePersonCommand.MESSAGE_SHARE_PERSON_SUCCESS,
-                SharePersonCommand.formatInput(Messages.format(personToShare)));
-
-        assertCommandSuccess(shareCommand, model, expectedMessage, expectedModel);
-    }
-
-    @Test
-    public void execute_invalidIndexFiltered_throwsCommandException() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-
-        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-        SharePersonCommand shareCommand = new SharePersonCommand(outOfBoundIndex);
-
-        CommandTestUtil.assertCommandFailure(shareCommand, model,
-                Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        assertCommandFailure(sharePersonCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
     @Test
     public void equals() {
-        SharePersonCommand shareCommand1 = new SharePersonCommand(INDEX_FIRST_PERSON);
-        SharePersonCommand shareCommand2 = new SharePersonCommand(INDEX_FIRST_PERSON);
+        SharePersonCommand shareFirstCommand = new SharePersonCommand(INDEX_FIRST_COMPANY, INDEX_FIRST_PERSON);
+        SharePersonCommand shareSecondCommand = new SharePersonCommand(INDEX_FIRST_COMPANY, INDEX_FIRST_PERSON);
 
         // same object -> returns true
-        assertTrue(shareCommand1.equals(shareCommand1));
+        assertTrue(shareFirstCommand.equals(shareFirstCommand));
 
         // same values -> returns true
-        assertTrue(shareCommand1.equals(shareCommand2));
+        SharePersonCommand shareFirstCommandCopy = new SharePersonCommand(INDEX_FIRST_COMPANY, INDEX_FIRST_PERSON);
+        assertTrue(shareFirstCommand.equals(shareFirstCommandCopy));
 
         // different types -> returns false
-        assertFalse(shareCommand1.equals(1));
+        assertFalse(shareFirstCommand.equals(1));
 
         // null -> returns false
-        assertFalse(shareCommand1.equals(null));
+        assertFalse(shareFirstCommand.equals(null));
+    }
 
-        // different index -> returns false
-        SharePersonCommand differentCommand = new SharePersonCommand(Index.fromOneBased(2));
-        assertFalse(shareCommand1.equals(differentCommand));
+    @Test
+    public void toStringMethod() {
+        SharePersonCommand sharePersonCommand = new SharePersonCommand(INDEX_FIRST_COMPANY, INDEX_FIRST_PERSON);
+        String expected = SharePersonCommand.class.getCanonicalName() + "{companyIndex=" + INDEX_FIRST_COMPANY
+                + ", personIndex=" + INDEX_FIRST_PERSON + "}";
+        assertEquals(expected, sharePersonCommand.toString());
     }
 }
