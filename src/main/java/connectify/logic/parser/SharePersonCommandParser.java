d030419b51d3b15914823f4bd257eefcb367025c
tayian <90464339+tayian@users.noreply.github.com>
2023-11-01 17:02:48 +0800

    Add logging for SharePersonCommandParser.java

--- src/main/java/connectify/logic/parser/SharePersonCommandParser.java
+++ src/main/java/connectify/logic/parser/SharePersonCommandParser.java
@@ -2,17 +2,23 @@ package connectify.logic.parser;
 
 import static connectify.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import connectify.commons.core.index.Index;
 import connectify.logic.commands.SharePersonCommand;
 import connectify.logic.parser.exceptions.ParseException;
 
 /**
- * Parses a Person object into a SharePersonCommand for execution.
+ * Parses a Company object into a ShareCompanyCommand for execution.
  */
 public class SharePersonCommandParser implements Parser<SharePersonCommand> {
 
+    private static final Logger logger = Logger.getLogger(SharePersonCommandParser.class.getName());
+
     /**
-     * Parses the given {@code Person} object and returns a SharePersonCommand object for execution.
+     * Parses the given {@code Company} object and returns a ShareCompanyCommand object for execution.
      * @throws ParseException if the parsing is unsuccessful
      */
     public SharePersonCommand parse(String args) throws ParseException {
@@ -25,6 +31,9 @@ public class SharePersonCommandParser implements Parser<SharePersonCommand> {
 
             Index companyIndex = ParserPersonUtil.parseIndex(splitArgs[0]);
             Index personIndex = ParserCompanyUtil.parseIndex(splitArgs[1]);
+
+            logger.log(Level.INFO,
+                    "Successfully parsed SharePersonCommand with keywords: " + Arrays.toString(splitArgs));
 
             return new SharePersonCommand(companyIndex, personIndex);
         } catch (ParseException pe) {
