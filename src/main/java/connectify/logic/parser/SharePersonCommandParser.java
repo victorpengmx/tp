33c77c4f0cd079cadbfe9365fc3c4280cdfe74b7
tayian <90464339+tayian@users.noreply.github.com>
2023-11-01 16:40:28 +0800

    Update SharePersonCommandParser.java

--- src/main/java/connectify/logic/parser/SharePersonCommandParser.java
+++ src/main/java/connectify/logic/parser/SharePersonCommandParser.java
@@ -7,18 +7,26 @@ import connectify.logic.commands.SharePersonCommand;
 import connectify.logic.parser.exceptions.ParseException;
 
 /**
- * Parses a Company object into a ShareCompanyCommand for execution.
+ * Parses a Person object into a SharePersonCommand for execution.
  */
 public class SharePersonCommandParser implements Parser<SharePersonCommand> {
 
     /**
-     * Parses the given {@code Company} object and returns a ShareCompanyCommand object for execution.
+     * Parses the given {@code Person} object and returns a SharePersonCommand object for execution.
      * @throws ParseException if the parsing is unsuccessful
      */
     public SharePersonCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserCompanyUtil.parseIndex(args);
-            return new SharePersonCommand(index);
+            String[] splitArgs = args.trim().split("\\s+");
+
+            if (splitArgs.length != 2) {
+                throw new ParseException(MESSAGE_INVALID_COMMAND_FORMAT);
+            }
+
+            Index companyIndex = ParserPersonUtil.parseIndex(splitArgs[0]);
+            Index personIndex = ParserCompanyUtil.parseIndex(splitArgs[1]);
+
+            return new SharePersonCommand(companyIndex, personIndex);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, SharePersonCommand.MESSAGE_USAGE), pe);
